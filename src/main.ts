5935e9df57bbc3de48e221cc8aef7881640f16a2
masih abedini <masih7816@gmail.com>
2025-01-31 21:50:14 +0330

    test(Prototype): add it's test into main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import prototypeTest from "./Creational/Prototype";
 import singletonMainTest from "./Creational/Singleton";
 import loggerTest from "./Creational/Singleton/Logger";
 
 singletonMainTest();
 loggerTest();
+prototypeTest();
