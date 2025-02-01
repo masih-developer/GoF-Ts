35503f85004779bdf61d3bbe31a3170967a8a8f8
masih abedini <masih7816@gmail.com>
2025-02-01 23:18:26 +0330

    test(Prototype/Shape): add it's test to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import prototypeTest from "./Creational/Prototype";
+import shapePrototypeTest from "./Creational/Prototype/Shape";
 import singletonMainTest from "./Creational/Singleton";
 import loggerTest from "./Creational/Singleton/Logger";
 
 singletonMainTest();
 loggerTest();
 prototypeTest();
+shapePrototypeTest();
